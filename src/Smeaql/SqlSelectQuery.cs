7f0d80ae62e8acaf4c28e05e0fbf0810d7f01b92
Shawn Beachy <shawnthebeachy@outlook.com>
2026-01-15 09:48:20 -0500

    Remove unused method.

--- src/Smeaql/SqlSelectQuery.cs
+++ src/Smeaql/SqlSelectQuery.cs
@@ -29,34 +29,4 @@ public sealed class SqlSelectQuery : SqlQueryBase<SqlSelectQuery>
     }
 
     internal override SqlSelectQuery This() => this;
-
-    public override string ToString()
-    {
-        /*var builder = new StringBuilder();
-        builder.Append("SELECT ");
-
-        foreach (var clause in Clauses)
-            if (clause is SelectClause)
-                clause.Write(builder);
-
-        if (!string.IsNullOrWhiteSpace(_table))
-            builder.Append($" FROM {_table}");
-
-        var startedWhere = false;
-
-        foreach (var clause in Clauses)
-            if (clause is WhereClause whereClause)
-            {
-                if (!startedWhere)
-                    builder.Append(" WHERE ");
-                else
-                    builder.Append($" {whereClause.WhereType.ToSql()} ");
-
-                startedWhere = true;
-                clause.Write(builder);
-            }
-
-        return builder.ToString();*/
-        return "";
-    }
 }

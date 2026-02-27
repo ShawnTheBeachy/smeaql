aec2cde7a258feb6825219721ede2a0bce4d3bdf
Shawn Beachy <shawnthebeachy@outlook.com>
2026-02-27 10:48:16 -0500

    Add sanitize extension method and tests.

---
+++ src/Smeaql/Helpers/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Smeaql.Helpers;
+
+internal static class StringExtensions
+{
+    public static ReadOnlySpan<char> Sanitize(this ReadOnlySpan<char> value)
+    {
+        if (value.Length < 1)
+            return value;
+
+        if (value[0] == '[' && value[^1] == ']')
+            return value;
+
+        if (value[0] == '"' && value[^1] == '"')
+            return value;
+
+        var arr = new char[value.Length + 2];
+        arr[0] = '[';
+        arr[^1] = ']';
+
+        for (var i = 0; i < value.Length; i++)
+            arr[i + 1] = value[i];
+
+        return arr;
+    }
+}
---
+++ test/Smeaql.Tests.Unit/Helpers/StringExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using Smeaql.Helpers;
+
+namespace Smeaql.Tests.Unit.Helpers;
+
+public sealed class StringExtensionsTests
+{
+    [Test]
+    public async Task Sanitize_ShouldDoNothing_WhenValueIsEmpty()
+    {
+        // Arrange.
+        const string value = "";
+
+        // Act.
+        var sanitized = new string(value.Sanitize());
+
+        // Assert.
+        await Assert.That(sanitized).IsEqualTo(value);
+    }
+
+    [Test]
+    public async Task Sanitize_ShouldDoNothing_WhenValueIsEnclosedInDoubleQuotes()
+    {
+        // Arrange.
+        const string value = "\"Value\"";
+
+        // Act.
+        var sanitized = new string(value.Sanitize());
+
+        // Assert.
+        await Assert.That(sanitized).IsEqualTo(value);
+    }
+
+    [Test]
+    public async Task Sanitize_ShouldDoNothing_WhenValueIsEnclosedInSquareBrackets()
+    {
+        // Arrange.
+        const string value = "[Value]";
+
+        // Act.
+        var sanitized = new string(value.Sanitize());
+
+        // Assert.
+        await Assert.That(sanitized).IsEqualTo(value);
+    }
+
+    [Test]
+    public async Task Sanitize_ShouldEncloseValueInSquareBrackets_WhenItIsNotAlreadyEnclosed()
+    {
+        // Arrange.
+        const string value = "My Value";
+
+        // Act.
+        var sanitized = new string(value.Sanitize());
+
+        // Assert.
+        await Assert.That(sanitized).IsEqualTo("[My Value]");
+    }
+}
